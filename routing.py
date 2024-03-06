9cdbbe810182a546ccf7716f1511274b71623752
Tombo-Combo77 <tcousins00@gmail.com>
2024-03-06 17:22:35 -0600

    Update routing.py

--- routing.py
+++ routing.py
@@ -70,8 +70,8 @@ class TAMU_Controller(Node):
             Pose2D, "/pose", self.pose_callback, 10)
 
         # Setting PID controller parameters
-        self.angle_PID = PID(kp=.5, ki=.00, kd=.00, setpoint=0)
-        self.linear_PID = PID(kp=.5, ki=.00, kd=.00, setpoint=0)
+        self.angle_PID = PID(kp=1, ki=.00, kd=.00, setpoint=0)
+        self.linear_PID = PID(kp=1, ki=.00, kd=.00, setpoint=0)
 
         # Setting bounds on the upper limits of the controller outputs
         self.max_rad = 17
